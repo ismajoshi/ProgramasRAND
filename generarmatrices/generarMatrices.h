8f24636a8fd2c7b39d155d680ffe43aaff24d868
topo <43886937+t0p0113@users.noreply.github.com>
2018-10-19 11:32:27 -0500

    codigos
    inge

--- generarmatrices/generarMatrices.h
+++ generarmatrices/generarMatrices.h
@@ -23,3 +23,38 @@ void generarMatriz(int mat[3][3]){
 		}
 	}
 }
+void multiplicarMatriz(int mat1[3][3], int mat2[3][3], int matR[3][3]){
+	int i,j,h;
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			matR[i][j]=0;
+		}
+	}
+	for(i=0;i<3;i++){
+		for(j=0;j<3;j++){
+			
+			for(h=0;h<3;h++){
+				matR[i][j]=matR[i][j]+(mat1[i][h]*mat2[h][j]);
+			}
+		}
+	}
+}
+
+char *ingresar (char *msg){
+	char dato[4];
+	char op;
+	int i=0;
+	printf("\n%s",msg);
+	while((op=getch)!=13){
+		if(op>='0'&&op<='9'){
+			printf("%c",op);
+			dato[i++]=op;
+		}
+	}
+	return dato;
+	
+}
+
+
+
+//TAREA MENU CON CURSORES
--- generarmatrices/main.cpp
+++ generarmatrices/main.cpp
@@ -3,17 +3,23 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <time.h>
+#include "generarMatrices.h"
+using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
-	int a[3][3], b[3][3];
+	int a[3][3], b[3][3],c[3][3];
 	generarMatriz(a);
 	imprimirMatriz(a);
 	printf("\n");
 	
 	generarMatriz(b);
 	imprimirMatriz(b);
+	
+	cout<<"\n";
+	multiplicarMatriz(a,b,c);
+	imprimirMatriz(c);
 	return 0;
 }
